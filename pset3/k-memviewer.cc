5849edb5bdbea7690bb756a498bcd6d676df8e63
Ryan Henry <ryanhenry@college.harvard.edu>
2022-10-25 22:08:01 -0400

    PSET3, ALL PHASES DONE

--- pset3/k-memviewer.cc
+++ pset3/k-memviewer.cc
@@ -178,7 +178,7 @@ uint16_t memusage::symbol_at(uintptr_t pa) const {
         if (vx == 0) {
             if (physpages[pa / PAGESIZE].used()) {
                 // Leaked page: used but not referenced by anything we know
-                log_printf("leak at: %x\n", pa);
+                // I want kernel to shut down if leak occurs!
                 assert(0 == 1);
                 return 'L' | 0x0300;
             } else {
--- pset3/kernel.cc
+++ pset3/kernel.cc
@@ -142,6 +142,7 @@ void* kalloc(size_t sz) {
 
 void kfree(void* kptr) {
     (void) kptr;
+    // Check if address is invalid
     if (kptr == nullptr || (uintptr_t) kptr == (uintptr_t) -1) {
         return;
     }
@@ -185,7 +186,12 @@ void process_setup(pid_t pid, const char* program_name) {
             assert(pa != nullptr);
 
             // Map virtual address to acquired physical address
-            int r = vmiter(ptable[pid].pagetable, va).try_map(pa, PTE_P | PTE_W | PTE_U);
+            // Detect read only perms
+            int perm = PTE_P | PTE_W | PTE_U;
+            if (!seg.writable()) {
+                perm = PTE_P | PTE_U;
+            }
+            int r = vmiter(ptable[pid].pagetable, va).try_map(pa, perm);
             assert(r == 0); 
         }
     }
@@ -196,7 +202,7 @@ void process_setup(pid_t pid, const char* program_name) {
 
     // Allocate and map stack segment
     // Compute process virtual address for stack page
-    uintptr_t stack_addr = MEMSIZE_VIRTUAL - PAGESIZE; //PROC_START_ADDR + PROC_SIZE * pid - PAGESIZE;
+    uintptr_t stack_addr = MEMSIZE_VIRTUAL - PAGESIZE;
     
     void* pa = kalloc(1);
     assert(pa != nullptr);
@@ -394,8 +400,6 @@ int syscall_page_alloc(uintptr_t addr) {
 }
 
 int syscall_fork() {
-    bool failure = false;
-    
     int pid = 1;
     while(pid <= 15 && ptable[pid].state != P_FREE) {
         pid++;
@@ -411,10 +415,10 @@ int syscall_fork() {
         return -1;
     }
 
+    // Copy memory
+    bool failure = false;
     vmiter srcit(current->pagetable, 0);
     vmiter dstit(ptable[pid].pagetable, 0);
-
-    // Copy memory
     for (; srcit.va() < MEMSIZE_VIRTUAL; srcit += PAGESIZE, dstit += PAGESIZE) {
         if ((srcit.va() < PROC_START_ADDR)) {
             int r = dstit.try_map(srcit.pa(), srcit.perm());
@@ -422,29 +426,41 @@ int syscall_fork() {
                 failure = true;
                 break;
             }
-        } else if (srcit.user() && srcit.writable()) {
-            // Allocate memory for copy
-            void* pa = kalloc(1);
-            
-            // Handle failure by freeing all previously alloc'd memory
-            if (pa == nullptr) {
-                failure = true;
-                break;
-            }
-            // Map child process memory
-            int r = dstit.try_map(pa, srcit.perm());
-            if (r != 0) {
-                kfree(pa);
-                failure = true;
-                break;
-            }
+        } else if (srcit.user()) {
+            // Copy memory if writable
+            if (srcit.writable()) {
+                // Allocate memory for copy
+                void* pa = kalloc(1);
+                
+                // Handle failure by freeing all previously alloc'd memory
+                if (pa == nullptr) {
+                    failure = true;
+                    break;
+                }
+                // Map child process memory
+                int r = dstit.try_map(pa, srcit.perm());
+                if (r != 0) {
+                    kfree(pa);
+                    failure = true;
+                    break;
+                }
 
-            // Copy memory from parent process to child process
-            memcpy(pa, (void*) srcit.pa(), PAGESIZE);
+                // Copy memory from parent process to child process
+                memcpy(pa, (void*) srcit.pa(), PAGESIZE);
+            // Share memory if read only
+            } else {
+                int r = dstit.try_map(srcit.pa(), srcit.perm());
+                if (r != 0) {
+                    failure = true;
+                    break;
+                }
+                physpages[srcit.pa() / PAGESIZE].refcount++;
+            }
+            
         }
-        
     }
 
+    // Clean up and exit upon failure
     if (failure) {
         dstit.find(PROC_START_ADDR);
         for (; dstit.va() < MEMSIZE_VIRTUAL; dstit += PAGESIZE) {
